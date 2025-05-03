56927eb953b870603b08923cc2ad4f57705ec3f2
Viraj_Rathva <141912973+viraj2023@users.noreply.github.com>
2025-05-03 14:31:55 +0530

    updated models

---
+++ backend/Models/Tour.js
@@ -0,0 +1,65 @@
+import mongoose from "mongoose";
+
+const tourSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    distance: {
+      type: Number,
+      required: true,
+    },
+    photo: {
+      type: String,
+      required: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    maxGroupSize: {
+      type: Number,
+      required: true,
+    },
+
+    reviews: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: "Review",
+      },
+    ],
+
+    featured: {
+      type: Boolean,
+      default: false,
+    },
+    itinerary: [
+      { 
+        day:Number,
+        description: [String],
+      },
+    ],
+    inclusions: [String],
+    exclusions: [String],
+    highlights: [String],
+    activities: [String],
+    thingsToCarry: [String],
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.Tour || mongoose.model("Tour", tourSchema);
