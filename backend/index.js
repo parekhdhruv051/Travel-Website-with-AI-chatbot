697ab5e394847fb1f57dac82d39dbc7b696a2af6
Viraj_Rathva <141912973+viraj2023@users.noreply.github.com>
2025-05-04 13:01:42 +0530

    updated routes

--- backend/index.js
+++ backend/index.js
@@ -52,7 +52,7 @@ app.use('/tour-images', express.static('images'));
 // ✅ Routes
 app.use('/api/v1/auth', authRoute);
 app.use('/api/v1/tours', tourRoute);
-app.use('/api/v1/users', userRoute);
+app.use('/api/v1/users', userRoute);  
 app.use('/api/v1/review', reviewRoute);
 app.use('/api/v1/booking', bookingRoute);
 app.use('/api/flights', serpapiRoute);
