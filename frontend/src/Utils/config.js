fea7fe8c427ec41810d99118edc9a85958049088
Viraj_Rathva <141912973+viraj2023@users.noreply.github.com>
2025-05-04 17:37:58 +0530

    updated high security with http"s"

--- frontend/src/Utils/config.js
+++ frontend/src/Utils/config.js
@@ -1,2 +1,2 @@
 
-export const BASE_URL = 'http://travel-website-with-ai-chatbot-production.up.railway.app/api/v1';
+export const BASE_URL = 'https://travel-website-with-ai-chatbot-production.up.railway.app/api/v1';
